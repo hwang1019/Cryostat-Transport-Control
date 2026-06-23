93dda27d324c423d80dad627c280d4fd1c6f4335
hwang1019 <haomin.wang.20@ucl.ac.uk>
2026-06-24 00:55:10 +0100

    Display live voltage output when sweeping the DAQ.
    When sweeping the voltage output from a DAQ channel, the live variation is now displayed.

--- Libraries/DAQ.py
+++ Libraries/DAQ.py
@@ -6,7 +6,7 @@ import nidaqmx
 import time
 import numpy as np
 
-def DAQset(channel, voltage):
+def DAQset(channel, voltage, display = False):
     '''
     Setting voltage output from a DAQ channel.
 
@@ -26,6 +26,9 @@ def DAQset(channel, voltage):
         task.ao_channels.add_ao_voltage_chan(f"{dev}/ao{cha}")
         task.write(-voltage) # -voltage because the output is opposite to the set value
 
+    if display:
+        print(f'Vout {channel} = {voltage:.2f} V.')
+
     return
 
 def DAQsweep(channel, start, end, rate):
@@ -42,14 +45,15 @@ def DAQsweep(channel, start, end, rate):
     points = int(round(abs(end - start)/(rate/3600)) + 1)
     volt = np.linspace(start, end, points)
     for v in volt:
-        DAQset(channel, v)
+        DAQset(channel, v, display = True)
         time.sleep(1)
     
-    print(f'Voltage output at {channel} is now {end} V.')
+    print(f'Voltage output at {channel} is now {end:.2f} V.')
 
 
 # Code for testing
-'''
-DAQset(650,0)
-DAQsweep(channel=650,start=1,end=0,rate=300)
-'''
+
+# DAQset(650,0)
+# DAQsweep(channel=650,start=0,end=0.5,rate=100)
+# DAQsweep(channel=650,start=0.5,end=0,rate=100)
+
